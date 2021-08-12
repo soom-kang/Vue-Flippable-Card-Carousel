2c7d815e2d8f37ced9f99ffb7f9fa32208b9542d
VannsKang <vannskang@gmail.com>
2021-08-12 20:55:30 +0900

    feat: apply responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			width: {
+				'card-web': '400px',
+				'card-mobile': '200px',
+			},
+			height: {
+				'card-web': '400px',
+				'card-mobile': '200px',
+			},
+		},
 	},
 	variants: {
 		extend: {},
